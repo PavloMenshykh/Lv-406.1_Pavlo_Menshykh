c0907298e6c9845d75878871e1e2bb3d85e1a54d
PavloMenshykh <pavlo.menshykh@gmail.com>
2019-04-29 12:48:58 +0300

    added ! at end of hello

--- pm_task_1.py
+++ pm_task_1.py
@@ -2,6 +2,6 @@ name_user = input ("What is your name?:\n")
 age = input ("How old are you?:\n")
 location = input  ("Where do you live?:\n")
 
-print ("Hello, "+name_user)
+print ("Hello, "+name_user+"!")
 print ("Your age is "+age)
 print ("You live in "+location)

bf9f6f203471225dbb15eca4e55db730f8b53a6b
PavloMenshykh <pavlo.menshykh@gmail.com>
2019-05-16 10:44:15 +0300

    commented fractal tree, added anchor point

--- Python fractal tree/fractal_tree_00.py
+++ Python fractal tree/fractal_tree_00.py
@@ -1,40 +1,60 @@
+#importing grasshopper python modules
 import rhinoscriptsyntax as rs
 import Rhino.Geometry as rg
 
+#importing generic modules
 from math import cos
 from math import sin
 from math import radians
 import random
 
+#getting starting point from anchorpoint
+x1 = anchorp.X
+y1 = anchorp.Y
+z1 = anchorp.Z
 
-x1 = 0
-y1 = 0
-z1 = 0
+#setting base variables
 x2 = x1
 y2 = y1
-z2 = 0
+z2 = z1
 
-anglerec = 0 
+#base angle
+anglerec = 0
+
+#list of lines that form the tree, this is the output variable 
 treelin = []
 
-def fractal(depth , x2, y2, z2, length, anglerec, angle, lvariation, aran, lran):
 
+#recursive function
+def fractal(depth , x2, y2, z2, length, anglerec, angle, lvariation, aran, lran):
+    
+    #test if depth>0
     if depth:
         
+        #defining random angle variation and length variation
         arn = random.uniform(-angle/100*aran, angle/100*aran)
         lrn = random.uniform(-length/100*lran, length/100*lran)
+        
+        #defining points
         x1 = x2
         y1 = y2
         z1 = z2
         x2 = x1 + (length+lrn)*cos(radians(anglerec))
         y2 = y1 + (length+lrn)*sin(radians(anglerec))
+        
+        #changing branch length dependant on branch depth
         length = length*lvariation
         
+        #building points
         stpoint = [x1, y1, z1]
         endpoint = [x2, y2, z2]
         
+        #building line between points
         treelin.append(rs.AddLine(stpoint, endpoint))
+        
+        #calling function with different angle parameter for branch splitting
         fractal(depth - 1 , x2, y2, z2, length, (anglerec+angle+arn), angle, lvariation, aran, lran)
         fractal(depth - 1 , x2, y2, z2, length, (anglerec-angle+arn), angle, lvariation, aran, lran)
         
+#first recursive function call
 fractal(depth, x2, y2, z2, length, anglerec, angle, lvariation, aran, lran)

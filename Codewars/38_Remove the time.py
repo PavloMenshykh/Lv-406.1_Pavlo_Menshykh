2854bd433dd8f1bfd988893f52b5464a6274d3b3
PavloMenshykh <pavlo.menshykh@gmail.com>
2019-05-16 10:02:56 +0300

    small change

--- Codewars/38_Remove the time.py
+++ Codewars/38_Remove the time.py
@@ -3,7 +3,7 @@ def shorten_to_date(long_date):
     return short[0]
 
 #https://www.codewars.com/kata/56b0ff16d4aa33e5bb00008e
-"""
+'''
 You're re-designing a blog and the blog's posts have the following format for showing the date and time a post was made:
 
 Weekday Month Day, time e.g., Friday May 2, 7pm
@@ -13,4 +13,4 @@ You're running out of screen real estate, and on some pages you want to display 
 Write a function, shortenToDate, that takes the Website date/time in its original string format, and returns the shortened format.
 
 Assume shortenToDate's input will always be a string, e.g. "Friday May 2, 7pm". Assume shortenToDate's output will be the shortened string, e.g., "Friday May 2".
-"""
+'''

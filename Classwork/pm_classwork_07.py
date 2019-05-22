2ae1bad04f222a2524007ef9e5ba8495ddf37303
PavloMenshykh <pavlo.menshykh@gmail.com>
2019-05-22 11:53:39 +0300

    classwork on classes

--- Classwork/pm_classwork_07.py
+++ Classwork/pm_classwork_07.py
@@ -1,29 +1,34 @@
-class Polygon:
-    def __init__(self, no_of_sides):
-        self.n = no_of_sides
-        self.sides = [0 for i in range(no_of_sides)]
+#Створити батьківський клас Figure з методами init: ініціалізується колір, 
+#                                                                                         get_color: повертає колір фігури,
+#                                                                                          info: надає інформацію про фігуру та колір,  
+#від якого наслідуються такі класи як Rectangle, Square, які мають інформацію про ширину, висоту фігури, метод square, який знаходить площу фігури.
 
-    def inputSides(self):
-        self.sides = [float(input("Enter side“ + str(i+1)+": ")) for i in range(self.n)]
 
-    def dispSides(self):
-        for i in range(self.n):
-            print("Side",i+1,"is",self.sides[i])
+class Figure:
+    def __init__(self, color, ftype):
+        self.get_color ="Figure color is %s" % color
+        self.info = "Figure is a %s, color is %s" % (ftype, color)
+    
+    def get_color(self, color):
+        self.get_color = color
 
-class Triangle(Polygon):
-    def __init__(self):
-        Polygon.__init__(self,3)  #super().__init__(3) 
+    def info(self, color, ftype):
+        self.info = color, ftype
 
-    def findArea(self):
-        a, b, c = self.sides
-        s = (a + b + c) / 2
-        area = (s*(s-a)*(s-b)*(s-c)) ** 0.5
-        print('The square of the triangle is %0.2f' %area)
+class Rectangle(Figure):
+    def __init__(self, color, a, b):
+        Figure.__init__(self, color, "rectangle")
+        self.square = "Figure area is %s" % str(a*b)
 
-t = Triangle()
+class Square(Figure):
+    def __init__(self, color, a):
+        Figure.__init__(self, color, "square")
+        self.square = "Figure area is %s" % str(a**2)
+    
 
-t.inputSides()
-
-t.dispSides()
-
-t.findArea()
+fig2 = Square("red", 3)
+fig3 = Rectangle("red", 3, 5)
+print(fig3.square)
+print(fig2.square)
+print(fig2.get_color)
+print(fig2.info)

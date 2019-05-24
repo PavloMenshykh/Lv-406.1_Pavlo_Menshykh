16b233bd881d3f173594995ae44b2d2b24659dc5
PavloMenshykh <pavlo.menshykh@gmail.com>
2019-05-24 13:04:59 +0300

    refining base script, next priority adding gravity to horizontalish branches

--- Python fractal tree/fractal_tree_10.py
+++ Python fractal tree/fractal_tree_10.py
@@ -25,13 +25,12 @@ import random
 #getting starting point from anchorpoint
 x1 = anchorp.X
 y1 = anchorp.Y
-z1 = anchorp.Z
+z1 = anchorp.Z-length #to generate starting vector
 
 #setting base variables
-x2 = x3 = x1
-y2 = y3 = y1
-z2 = z1 
-z3 = length
+x2 = x1
+y2 = y1 
+z2 = 0 #to generate starting vector
 
 #base angle
 anglerec = 0
@@ -39,11 +38,10 @@ anglerech = 0
 
 #list of lines that form the tree, this is the output variable 
 treelin = {}
-
 anglest = []
 
 #recursive function
-def fractal(depth, x2, y2, z2, x3, y3, z3, length, anglerec, angle, lvariation, aran, lran, anglerech, angleh):
+def fractal(depth, x1, y1, z1, x2, y2, z2, length, anglerec, angle, lvariation, aran, lran, anglerech, angleh, branches):
     
     #test if depth>0
     if depth:
@@ -53,7 +51,7 @@ def fractal(depth, x2, y2, z2, x3, y3, z3, length, anglerec, angle, lvariation, 
         lrn = random.uniform(-length/100*lran, length/100*lran)
         
         #defining horizontal rotation angles
-        ahor = random.sample(range(0,360), 4)
+        ahor = random.sample(range(0,360), branches)
         
         #removing numbers within tolerance
         ahr = rs.CullDuplicateNumbers(ahor, angleh)
@@ -66,68 +64,48 @@ def fractal(depth, x2, y2, z2, x3, y3, z3, length, anglerec, angle, lvariation, 
         
         #previous branch vector
         
-        vecst = rg.Point3d(x2, y2, z2)
-        vecend = rg.Point3d(x3, y3, z3)
+        vecst = rg.Point3d(x1, y1, z1)
+        vecend = rg.Point3d(x2, y2, z2)
         
-        movevec = ghc.Vector2Pt(vecst, vecend, True)[0]
+        movevec = ghc.Vector2Pt(vecst, vecend, True)[0] #returns vector and it's length
         
         #perpendicular vector rotation plane
         
-        rotplane3 = ghc.PlaneNormal(vecend, movevec)[0]
+        rotplane3 = ghc.PlaneNormal(vecend, movevec) #creates plane perpendicular to vector
         plns = ghc.DeconstructPlane(rotplane3) #origin, x, y, z
-        rotplane = ghc.ConstructPlane(plns[0], plns[3], plns[2])
+        rotplane = ghc.ConstructPlane(plns[0], plns[3], plns[2]) #constructing new plane switching x and y planes to make perpendicular
         
+        #generating perpendicular vector
         vecperp = ghc.Rotate(movevec, radians(90), rotplane)[0]
         
-        
-        #defining points
-        x1 = x2
-        y1 = y2
-        z1 = z2
-        x2 = x3
-        y2 = y3
-        z2 = z3
-        x3 = x3
-        leny = (length+lrn)*sin(radians(anglerec))
-        lenz = (length+lrn)*cos(radians(anglerec))
+        #generating vector amplitudes
+        leny = (length+lrn)*sin(radians(anglerec+arn))
+        lenz = (length+lrn)*cos(radians(anglerec+arn))
         ampy = ghc.Amplitude(vecperp, leny)
         ampz = ghc.Amplitude(movevec, lenz)
-        #y3 = ghc.Move(y2, ampy)[0]
-        #z3 = ghc.Move(z2, ampz)[0]
-        #ghc.Amplitude(vector, length)
-        #ghc.Move(geometry, vector)[0]
-        
-        #y3 = y2 + (length+lrn)*sin(radians(anglerec))
-        #z3 = z2 + (length+lrn)*cos(radians(anglerec))
-        
         
         #changing branch length dependant on branch depth
         length = length*lvariation
         
         #building points
-        stpoint = vecend 
-        #endpoint = rg.Point3d(x3, y3, z3)
-        endpoint1 = ghc.Move(stpoint, ampz)[0]
-        endpoint = ghc.Move(endpoint1, ampy)[0]
-        
-        #endpoint = ghc.PointOriented(rotplane, 0, y3, z3)
+        endpoint1 = ghc.Move(vecend, ampz)[0] #returns moved object and transformation data
+        endpoint = ghc.Move(endpoint1, ampy)[0] #returns moved object and transformation data
         
         #rotating point in a cone fashion
-        
-        rotpoint = ghc.Rotate3D(endpoint, anglerech, vecend, movevec)[0]
+        rotpoint = ghc.Rotate3D(endpoint, anglerech, vecend, movevec)[0] #returns rotated geometry and transformation data
         
         #building line between points
-        linegeo = rg.Line(stpoint, rotpoint)
+        linegeo = rg.Line(vecend, rotpoint)
+        
+        #setting coords for next branch
+        x1 = x2
+        y1 = y2
+        z1 = z2
         
         #getting xyz from rotated point
-        
-        x3 = rg.Point3d(rotpoint)[0]
-        y3 = rg.Point3d(rotpoint)[1]
-        z3 = rg.Point3d(rotpoint)[2]
-        
-        x2 = rg.Point3d(vecend)[0]
-        y2 = rg.Point3d(vecend)[1]
-        z2 = rg.Point3d(vecend)[2]
+        x2 = rg.Point3d(rotpoint)[0]
+        y2 = rg.Point3d(rotpoint)[1]
+        z2 = rg.Point3d(rotpoint)[2]
         
         #building a dict of lines with depth as key, and corresponding lines as values
         if depth not in treelin.keys():
@@ -138,12 +116,12 @@ def fractal(depth, x2, y2, z2, x3, y3, z3, length, anglerec, angle, lvariation, 
         #calling function with different angle parameter for branch splitting
         #calling as many branches as spread within tolerance
         for aa in ahr:
-            fractal(depth - 1 , x2, y2, z2, x3, y3, z3, length, angle, angle, lvariation, aran, lran, aa, angleh)
+            fractal(depth - 1 , x1, y1, z1, x2, y2, z2, length, angle, angle, lvariation, aran, lran, aa, angleh, branches)
         
 
         
 #first recursive function call
-fractal(depth, x2, y2, z2, x3, y3, z3, length, anglerec, angle, lvariation, aran, lran, anglerech, angleh)
+fractal(depth, x1, y1, z1, x2, y2, z2, length, anglerec, angle, lvariation, aran, lran, anglerech, angleh, branches)
 
 #sort dict of branch lines
 treelinsorted = {}
@@ -163,7 +141,9 @@ pgons = []
 
 #create ngon branch depth dependant
 def ngons(args):
+    
     nn, length, lvariation, kk, radtolen, mngon, depth = args
+    
     stpnt = ghc.EndPoints(nn)[0] #returns list of two points, start and end
     endpnt = ghc.EndPoints(nn)[1]
     vect = ghc.Vector2Pt(stpnt, endpnt, False)[0] #returns list with vector and vector length
@@ -186,15 +166,15 @@ def ngons(args):
     return ghc.CapHoles(geo) #caps ends on the extruded brep
 
 #iterate over branches and run ngons func
-for kk in treelinsorted.keys():
-    for nn in treelinsorted[kk]:
-        args = [nn, length, lvariation, kk, radtolen, mngon, depth]
-        pgons.append(ngons(args))
+def makegeo(treelinsorted):
+    for kk in treelinsorted.keys():
+        for nn in treelinsorted[kk]:
+            args = [nn, length, lvariation, kk, radtolen, mngon, depth]
+            pgons.append(ngons(args))
+            
+ghp.run(makegeo, [treelinsorted], True)
 
 def joiner(breps):
     return ghc.SolidUnion(breps)
 
 #joined = joiner(pgons)
-
-treelinout = treelin[1]
-treelinout += treelin[2]
